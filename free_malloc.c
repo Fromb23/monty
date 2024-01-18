b8b6cf4a8b81eede6cddadc1318d2402dd3b9590
Fromb23 <rombo.programming@gmail.com>
2024-01-18 04:31:29 -0800

    free malloc betty stled

--- free_malloc.c
+++ free_malloc.c
@@ -1,5 +1,10 @@
 #include "monty.h"
 
+/**
+ * free_malloc - Frees the memory allocated for a stack
+ * @head: Pointer to the head of the stack
+ **/
+
 void free_malloc(stack_t **head)
 {
 	stack_t *current_node = *head, *temp_node;
